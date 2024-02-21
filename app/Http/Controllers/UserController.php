5a986e22dd8a0c6b5adc118a46a71650a21475ea
belasriiimad <imadbelasri@gmail.com>
2024-02-21 09:48:53 +0000

    first commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Http\Requests\AuthUserRequest;
+use App\Http\Requests\StoreUserRequest;
+
+class UserController extends Controller
+{
+    public function store(StoreUserRequest $request)
+    {
+        if($request->validated()) {
+            User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+            ]);
+
+            return response()->json([
+                'message' => 'Account has been created successfully.'
+            ]);
+        }
+    }
+
+    public function auth(AuthUserRequest $request)  
+    {
+        if($request->validated()) {
+            $user = User::whereEmail($request->email)->first();
+            if(!$user || Hash::check($request->password, $user->password)) {
+                return response()->json([
+                    'error' => 'These credentials do not match any of our records.'
+                ]);
+            }else {
+                return response()->json([
+                    'user' => $user,
+                    'message' => 'Logged in successfully.',
+                    'currentToken' => $user->createToken('new_user')->plainTextToken
+                ]);
+            }
+        }
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+        return response()->json([
+            'message' => 'Logged out successfully.'
+        ]);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Http\Controllers\UserController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('user', function(Request $request) {
+        return [
+            'user' => $request->user(),
+            'currentToken' => $request->bearerToken()
+        ];
+    });
+    Route::post('user/logout', [UserController::class, 'logout']);
+});
+
+Route::post('user/login', [UserController::class, 'auth']);
+Route::post('user/register', [UserController::class, 'store']);
